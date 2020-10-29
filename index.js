fcc2904e809b1e5b32e75c77afa6fd70bdcd4b4f
Mohamed <spider_mo@hotmail.nl>
2020-10-29 13:16:53 +0100

    Wat veranderingen

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
+// Alle variabelen importen van de file variables.js
 import {api, areadesc, laadPunten, parkeerPlaatsen, maxInrijHoogte} from '/variables.js'
-// const api = 'https://opendata.rdw.nl/resource/9c54-cmfx.json'
-// const areadesc = 'areadesc'
-// const laadPunten = 'aantal_laad_punten'
-// const parkeerPlaatsen = 'aantal_parkeer_plaatsen'
-// const maxInrijHoogte = 'maximale_inrij_hoogte'
+const api = 'https://opendata.rdw.nl/resource/9c54-cmfx.json'
+const areadesc = 'areadesc'
+const laadPunten = 'aantal_laad_punten'
+const parkeerPlaatsen = 'aantal_parkeer_plaatsen'
+const maxInrijHoogte = 'maximale_inrij_hoogte'
 
+//Gebruik functie getData() en zet daarin de api endpoint, vervolgens de functie filteredData gebruikt om alleen de data te krijgen die ik wil
 getData(api)
     .then(data => {
-    // const areadescArray = filteredData(data, areadesc)
-    // const laadpuntenArray = filteredData(data, laadPunten)
-    // const parkeerPlaatsenArray = filteredData(data, parkeerPlaatsen)
-    // const maxInrijHoogteArray = filteredData(data, maxInrijHoogte)
-    // console.log('area descriptie', areadescArray, 'laad punten', laadpuntenArray, 'parkeer plaatsen', parkeerPlaatsenArray, 'max inrij hoogte', maxInrijHoogteArray)
-    // console.table([areadescArray, laadpuntenArray, parkeerPlaatsenArray, maxInrijHoogteArray])
+    const areadescArray = filteredData(data, areadesc)
+    const laadpuntenArray = filteredData(data, laadPunten)
+    const parkeerPlaatsenArray = filteredData(data, parkeerPlaatsen)
+    const maxInrijHoogteArray = filteredData(data, maxInrijHoogte)
+    console.log('area descriptie', areadescArray, 'laad punten', laadpuntenArray, 'parkeer plaatsen', parkeerPlaatsenArray, 'max inrij hoogte', maxInrijHoogteArray)
     
-        console.log(data)
-        // if (!question.includes('toegankelijk_voor_gehandicapten')){
-        //     question.push('toegankelijk_voor_gehandicapten')
-        // } return question
     }
-    // console.log(improvedDataSet)
-    
 )
 
 
-
+// async function die de api endpoint (die de parameter url is) fetched en daarna de response converteerd naar json die opgeslagen wordt in de variabel data
 async function getData(url) {
 const response = await fetch(url);
 const data = await response.json();
 return data;
 }
 
+// functie die twee parameters in krijgt dataArray en de column hiervan, vervolgens maak ik een nieuwe array met behulp van de .map() method die de functie doorstaan
 function filteredData (dataArray, column) {
     return dataArray.map(result => result[column])
 }
 
-// const url = 'https://opendata.rdw.nl/resource/9c54-cmfx.json'
-// fetch(url)
-//     .then(
-//         response => {
-//             console.log(response)
-//             response.json()
-//             .then( data => {
-//                 console.log(data)
-//             }
-//                 )
-//         }
-//     )
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-// Variabel aangemaakt om data functie van andere file op te roepen
+// Variabel aangemaakt om data functie van andere file (studenten dataset) op te roepen
 var deData = data()
 
 // Variabel om nieuwe gefilterde array in te stoppen
@@ -9,43 +9,11 @@ for (answer of deData.data){
     favorieteKleur.push(answer.lievelingskleur)
 }
 
-// Probeerde hier alle antwoorden die niet een kleur zijn uit te filteren uit de favorieteKleur array, lukde me bijna maar was lelijk
-
-// if(favorieteKleur.includes("0", "", "/")) {
-//  return null
-// }
-// let nul = {"0" : null, "" : null, "/" : null}
-// favorieteKleur.replace(/0/g, null).replace(/""/g, null).replace(/"/")
-
 //Variabel die alle antwoorden die niet een kleur zijn in heeft
 let nonUsableAnswers = ["/", "", "0"]
 
 // Gebruik hier de .filter method om alleen de antwoorden die niet de nonUsableAnswer in hebben, te gebruiken in de favorieteKleur array
 let newArray = favorieteKleur.filter(f => !nonUsableAnswers.includes(f))
-
-// zat hier te strugglen omdat ik geen aangepaste array kreeg, ging toen de les opnieuw kijken en kwam erachter dat een functie iets 1 keer maar returned
-
-// function test(shizzle) {
-//     for(answer of shizzle) {
-//     if (!answer.includes('#')) {
-//         return `#${answer}`
-//     }
-// }
-// }
-
-// moet het dus storen in een nieuwe array
-
-// function test(Answer) {
-//     let newArrayAnswer = []
-//     for(answer of Answer) {
-//     if (!answer.includes('#')) {
-//         newArrayAnswer.push(`#${answer}`)
-//     } else if (answer.includes('#')){
-//         newArrayAnswer.push(answer)
-//     }
-// }
-// return newArrayAnswer
-// }
 
 // Brian ging me helpen om dit een stuk makkelijker te maken met behulp van map, zie zelf de logica wel in! Nog eerst wel alle antwoorden die niet HEX waren 
 // gehardcode naar HEX
